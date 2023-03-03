5820ac90949a0dddad1faa35685e81adead5eb5d
Antonio Guimarães <antonio.guimaraes@sou.inteli.edu.br>
2023-03-03 11:32:07 -0300

    feat: adiciona sprite sandra tela escolha e tilesets lvl1

--- src/Global/Global.gd
+++ src/Global/Global.gd
@@ -4,6 +4,6 @@ extends Node
 #Variáveis globais
 var jogador_escolhido = 0 #Variavel para personagem a ser escolhido
 var status = 1 #Variavel a fim de saber se está jogando ou não
-var moedas = 0
+var moedas = 0 #Var qntd de moedas
 
 
